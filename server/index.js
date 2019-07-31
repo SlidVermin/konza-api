a9504f30068bc89c7dd92ab7e33bc7ce2725a93f
Adrian Baldovino <abaldovino03@gmail.com>
2019-07-31 18:16:56 -0500

    Finish API creation

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const api = require("./api/v1");
+
+const app = express();
+
+app.use("/api/v1", api);
+app.use("/api", api);
+
+// Not route found middleware
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: "Resource not found",
+  });
+});
+
+// Error middleware
+
+app.use((err, req, res, next) => {
+  const { message, statusCode = 500 } = err;
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
+});
+
+module.exports = app;
